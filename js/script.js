436dbd3e5dfde2ec3985042cf4de9f37ab0f5abb
oninotna <antoninomusarratubbi@gmail.com>
2025-04-23 15:10:06 +0200

    check run ajax call

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const emailRandomGenerate = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
+axios.get(emailRandomGenerate).then(response => {
+    emailGenerate = response.data.response;
+  console.log(emailGenerate);  
+});
 
 
