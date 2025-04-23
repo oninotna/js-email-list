18ffa7f25874526c90f7c1786e155ba04fde292b
oninotna <antoninomusarratubbi@gmail.com>
2025-04-23 15:34:13 +0200

    implemented ajax call in a function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+//! FUNCTION
+function listGenerate (listLength, callAPI) {
+    const list = [];
+    for (let i = 0; i < listLength; i++) {
+        axios.get(callAPI).then(response => {
+           emailGenerate = response.data.response;
+           list.push(emailGenerate);
+        });       
+    };
+
+    return list;
+};
+
 const emailRandomGenerate = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-axios.get(emailRandomGenerate).then(response => {
-    emailGenerate = response.data.response;
-  console.log(emailGenerate);  
-});
+console.log(listGenerate(10, emailRandomGenerate));
 
 
